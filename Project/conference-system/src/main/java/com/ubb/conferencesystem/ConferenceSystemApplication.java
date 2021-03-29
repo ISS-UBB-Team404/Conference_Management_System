a05cab1ae63ffc2fd18c1b975aa6dc3e3e296abb
Andrei <andreigabor36@gmail.com>
2021-03-30 00:58:21 +0300

    Hibernate is working

--- Project/conference-system/src/main/java/com/ubb/conferencesystem/ConferenceSystemApplication.java
+++ Project/conference-system/src/main/java/com/ubb/conferencesystem/ConferenceSystemApplication.java
@@ -1,7 +1,9 @@
 package com.ubb.conferencesystem;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ConferenceSystemApplication {
@@ -10,4 +12,11 @@ public class ConferenceSystemApplication {
 		SpringApplication.run(ConferenceSystemApplication.class, args);
 	}
 
+	@Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student andrei = new Student("Andrei", "Gabor", "abc@gmail.com", 20);
+            studentRepository.save(andrei);
+        };
+    }
 }
