2a2d8ef742c2853751ef1bd818d0bcb96fef81b0
ghetinavlad <73642253+ghetinavlad@users.noreply.github.com>
2021-04-13 20:31:10 +0300

    added some styling

--- Project/conference-ui/src/app/app.module.ts
+++ Project/conference-ui/src/app/app.module.ts
@@ -5,10 +5,10 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import {RouterModule} from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { EnterComponent } from './enter/enter.component';
 import { PaperSubmitComponent } from './paper-submit/paper-submit.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
