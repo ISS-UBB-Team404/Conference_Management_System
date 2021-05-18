f3a6f806414542ff1b918c2d050ce27db1d71b97
ghetinavlad <73642253+ghetinavlad@users.noreply.github.com>
2021-05-18 17:51:02 +0300

    section styling done

---
+++ Project/conference-ui/src/app/submit-paper/submit-paper.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SubmitPaperComponent } from './submit-paper.component';
+
+describe('SubmitPaperComponent', () => {
+  let component: SubmitPaperComponent;
+  let fixture: ComponentFixture<SubmitPaperComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SubmitPaperComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SubmitPaperComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Project/conference-ui/src/app/submit-paper/submit-paper.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-submit-paper',
+  templateUrl: './submit-paper.component.html',
+  styleUrls: ['./submit-paper.component.css']
+})
+export class SubmitPaperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
