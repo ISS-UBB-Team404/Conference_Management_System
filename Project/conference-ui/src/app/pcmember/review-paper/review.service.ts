8da624b6e0316ce6f9b37fc9872f9b8672f56473
Goian Tudor <varutudor@gmail.com>
2021-05-25 09:46:51 +0300

    Added session storage for reviews.

--- Project/conference-ui/src/app/pcmember/review-paper/review.service.ts
+++ Project/conference-ui/src/app/pcmember/review-paper/review.service.ts
@@ -6,15 +6,23 @@ import {Review} from './review';
 })
 export class ReviewService {
   allReviews: Review[] = [];
+  ALL_REVIEWS_SESSION_KEY = 'allReviews';
 
   constructor() {
   }
 
   addReview(review: Review): void {
-    this.allReviews.push(review);
+    if (localStorage.getItem(this.ALL_REVIEWS_SESSION_KEY) === null){
+      this.allReviews.push(review);
+      sessionStorage.setItem(this.ALL_REVIEWS_SESSION_KEY, JSON.stringify(this.allReviews));
+    }else {
+      this.allReviews = JSON.parse(sessionStorage.getItem(this.ALL_REVIEWS_SESSION_KEY));
+      this.allReviews.push(review);
+      sessionStorage.setItem(this.ALL_REVIEWS_SESSION_KEY, JSON.stringify(this.allReviews));
+    }
   }
 
   getAllReviews(): Review[] {
-    return this.allReviews;
+    return JSON.parse(sessionStorage.getItem(this.ALL_REVIEWS_SESSION_KEY));
   }
 }
